c858f2166dcdd198b6ea3549fcf9b9d0f0001933
vnFuhung2903 <hung29032004@gmail.com>
2026-01-08 21:10:59 +0700

    feat: update MultiMatchSearchRequest

--- backend/app/dto/request/multi_match_search_request.py
+++ backend/app/dto/request/multi_match_search_request.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 
 class MultiMatchSearchRequest(BaseModel):
     query: str                  # từ khóa tìm kiếm
-    fields: List[str]           # list các field để multi_match
+    fields: Optional[List[str]] = ["title", "link", "summary"]  # list các field để multi_match, default to title and summary
     page: Optional[int] = 1    # index bắt đầu (offset)
     size: Optional[int] = 10    # số document trả về
